62a63fc07917eea61c11a833e2bf8d8427e4b8b0
2018csb1082 <daksh1sharma1@gmail.com>
2025-06-10 20:24:42 +0530

    Retrieved and displayed weather data, needs css magic

--- src/components/WeatherContainer/WeatherContainer.jsx
+++ src/components/WeatherContainer/WeatherContainer.jsx
@@ -4,7 +4,28 @@ import './WeatherContainer.css'
 export default function WeatherContainer(weatherData) {
   return (
     <div className='WeatherContainer'>
-      {JSON.stringify(weatherData)}
+      <div className='WeatherRow'>
+        <div className='WeatherObject'>
+          <img src={weatherData.weatherData.current.condition.icon} height='100%' width='100%'/>
+        </div>
+        <div className='WeatherObject'>
+          {weatherData.weatherData.location.name}
+        </div>
+        <div className='WeatherObject'>
+          {new Date(weatherData.weatherData.location.localtime_epoch).toDateString()}
+        </div>
+      </div>
+      <div className='WeatherRow'>
+        <div className='WeatherObject'>
+          {weatherData.weatherData.current.condition.text}
+        </div>
+        <div className='WeatherObject'>
+          {weatherData.weatherData.current.temp_c} C
+        </div>
+        <div className='WeatherObject'>
+          AQI: {weatherData.weatherData.current.air_quality.co}
+        </div>
+      </div>
     </div>
   );
 };
