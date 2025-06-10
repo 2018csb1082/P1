31c6c7026c0a94c556de0da72d73c13be7f1bff9
2018csb1082 <daksh1sharma1@gmail.com>
2025-06-10 13:22:13 +0530

    Fetching weather data

--- src/components/WeatherApp/WeatherApp.jsx
+++ src/components/WeatherApp/WeatherApp.jsx
@@ -1,10 +1,44 @@
 import { useState , useEffect} from 'react'
+import axios from 'axios'
+import './WeatherApp.css'
 
 export default function WeatherApp() {
+  const [weatherData, setWeatherData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
 
-  return (
-    <div>
-      Hello World!
+  useEffect(() => {
+    const fetchWeather = async () => {
+      try {
+        const result = await axios.get('http://api.weatherapi.com/v1/current.json', {
+          params: {
+            key: '32b3f84daada42eb86970134251006',
+            q: 'Chandigarh',
+            aqi: 'yes'
+          }
+        });
+        setWeatherData(result.data);
+        setLoading(false);
+      } catch(error) {
+        setError('Error: ' + error);
+        setLoading(false);
+      }
+    };
+    fetchWeather();
+  }, []);
+
+  if (loading) {
+    return(<div className='WeatherLoading'>Loading...</div>);
+  }
+
+  if (error != "") {
+    return(<div className='WeatherError'>{error}</div>);
+  } 
+
+  return(
+    <div className='WeatherContainer'>
+      <div>We got weather!</div>
+      <div>{JSON.stringify(weatherData)}</div>
     </div>
   )
 }
