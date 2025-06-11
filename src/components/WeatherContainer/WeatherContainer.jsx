913c249a09e72fa27177bf03575b53b7838ef256
2018csb1082 <daksh1sharma1@gmail.com>
2025-06-11 17:13:55 +0530

    CSS changes done

--- src/components/WeatherContainer/WeatherContainer.jsx
+++ src/components/WeatherContainer/WeatherContainer.jsx
@@ -1,29 +1,39 @@
 import { useState } from 'react'
 import './WeatherContainer.css'
 
+import { FaWind } from "react-icons/fa";
+import { WiDust } from "react-icons/wi";
+import { WiHumidity } from "react-icons/wi";
+
 export default function WeatherContainer(weatherData) {
   return (
     <div className='WeatherContainer'>
-      <div className='WeatherRow'>
-        <div className='WeatherObject'>
-          <img src={weatherData.weatherData.current.condition.icon} height='100%' width='100%'/>
-        </div>
-        <div className='WeatherObject'>
-          {weatherData.weatherData.location.name}
-        </div>
-        <div className='WeatherObject'>
-          {new Date(weatherData.weatherData.location.localtime_epoch).toDateString()}
+      <div className='WeatherRow heading'>
+        <div className='WeatherObject location'>
+          {weatherData.weatherData.location.name.toUpperCase()}
         </div>
       </div>
       <div className='WeatherRow'>
-        <div className='WeatherObject'>
-          {weatherData.weatherData.current.condition.text}
+        <div className='WeatherObject temp'>
+          <div>{weatherData.weatherData.current.temp_c}&deg;</div>
+          <div className='cond'>{weatherData.weatherData.current.condition.text}</div>
         </div>
         <div className='WeatherObject'>
-          {weatherData.weatherData.current.temp_c} C
+          <img src={weatherData.weatherData.current.condition.icon} height='100%' width='100%'/>
         </div>
-        <div className='WeatherObject'>
-          AQI: {weatherData.weatherData.current.air_quality.co}
+      </div>
+      <div className='WeatherRow'>
+        <div className='WeatherObject details'>
+          <div><FaWind /> Wind</div>
+          <div>{weatherData.weatherData.current.wind_kph} <span className='unit'>kmph</span></div>
+        </div>
+        <div className='WeatherObject details'>
+          <div><WiDust /> AQI</div>
+          <div>{Math.round(weatherData.weatherData.current.air_quality.pm2_5)}</div>
+        </div>
+        <div className='WeatherObject details'>
+          <div>Humidity</div>
+          <div>{weatherData.weatherData.current.humidity} <span className='unit'>%</span></div>
         </div>
       </div>
     </div>
