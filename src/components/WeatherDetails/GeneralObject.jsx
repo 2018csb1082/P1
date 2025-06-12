15d8adcc7cc373fe77ecdf798534067230385224
2018csb1082 <daksh1sharma1@gmail.com>
2025-06-12 13:04:45 +0530

    cleaning up CSS

--- src/components/WeatherDetails/GeneralObject.jsx
+++ src/components/WeatherDetails/GeneralObject.jsx
@@ -3,7 +3,7 @@ import './WeatherDetails.css'
 function GeneralObject({icon, detail_heading, detail_num, unit}) {
   return (
     <div className='WeatherObject details'>
-      <div>{icon} {detail_heading}</div>
+      <div>{detail_heading} {icon}</div>
       <div>{detail_num} <span className='unit'>{unit}</span></div>
     </div>
   );
