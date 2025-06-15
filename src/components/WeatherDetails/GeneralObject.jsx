297c189664916b2c31b99e529ecc74956a9b77f4
2018csb1082 <daksh1sharma1@gmail.com>
2025-06-15 14:06:57 +0530

    changes for deploying

--- src/components/WeatherDetails/GeneralObject.jsx
+++ src/components/WeatherDetails/GeneralObject.jsx
@@ -4,7 +4,7 @@ function GeneralObject({icon, detail_heading, detail_num, unit}) {
   return (
     <div className='WeatherObject details'>
       <div>{detail_heading} {icon}</div>
-      <div>{detail_num} <span className='unit'>{unit}</span></div>
+      <div>{detail_num}<span className='unit'>{unit}</span></div>
     </div>
   );
 }
